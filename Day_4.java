f8e0938089155116407f1e3054f2710babd340f6
Fatih ASA <44146915+Fatihasa@users.noreply.github.com>
2020-04-21 23:16:58 +0200

    Update Day_4.java

--- Day_4.java
+++ Day_4.java
@@ -1,17 +1,12 @@
-/Temp easy solution, I will try to find optimized method later
+//Temp easy solution, I will try to find optimized method later
 /*
-
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
 Example:
-
 Input: [0,1,0,3,12]
 Output: [1,3,12,0,0]
 Note:
-
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
-
 */
 import java.util.ArrayList;
 
